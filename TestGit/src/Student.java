7eeb55664ebfc44f7f014455586dca2b45d85861
liuyang <sliuyang_java@163.com>
2021-02-25 09:27:31 +0800

    给Student类添加了age，name属性

--- TestGit/src/Student.java
+++ TestGit/src/Student.java
@@ -1,10 +1,11 @@
 /**
- * ClassName: Student
- * Package: PACKAGE_NAME
- * Description:
- *
- * @date: 2021/2/25 9:17
- * @author: LiuYang
- */
+* ClassName: Student
+* Package: PACKAGE_NAME
+* Description:
+* @date: 2021/2/25 9:17
+* @author: LiuYang
+*/
 public class Student {
+    private int age;
+    private String name;
 }
